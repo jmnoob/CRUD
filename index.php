9fa87b0d3ec0b1ee8bd6152cbf6fca3a5948b075
judeMahilum <31527646+jmnoob@users.noreply.github.com>
2021-10-07 15:49:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,46 +9,42 @@
 </head>
 <body>
 	<div class="container">
-		
-		<form action="php/create.php"
+		<form action="php/create.php" 
 		      method="post">
+            
+		   <h4 class="display-4 text-center">Create</h4><hr><br>
+		   <?php if (isset($_GET['error'])) { ?>
+		   <div class="alert alert-danger" role="alert">
+			  <?php echo $_GET['error']; ?>
+		    </div>
+		   <?php } ?>
+		   <div class="form-group">
+		     <label for="name">Name</label>
+		     <input type="name" 
+		           class="form-control" 
+		           id="name" 
+		           name="name" 
+		           value="<?php if(isset($_GET['name']))
+		                           echo($_GET['name']); ?>" 
+		           placeholder="Enter name">
+		   </div>
 
-			   <h4 class="display-4 text-center">Create</h4><hr><br>
-			   <?php if (isset($_GET['error'])) { ?>
-			   <div class="alert alert-danger" role="alert">
-				  <?php echo $_GET['error']; ?>
-			   </div>
-			   <?php } ?>
-			  <div class="form-group">
-			    <label for="name">Name</label>
-			    <input type="name" 
-			    	   class="form-control" 
-			    	   id="name" 
-			    	   name="name"
-			    	   value="<?php if(isset($_GET['name']))
-			    						echo($_GET['name']); ?>" 
-			    	   placeholder="Enter Name">
-			  </div>
+		   <div class="form-group">
+		     <label for="email">Email</label>
+		     <input type="email" 
+		           class="form-control" 
+		           id="email" 
+		           name="email" 
+		           value="<?php if(isset($_GET['email']))
+		                           echo($_GET['email']); ?>"
+		           placeholder="Enter email">
+		   </div>
 
-			  <div class="form-group">
-			    <label for="email">Email</label>
-			    <input type="email" 
-			    	   class="form-control" 
-			    	   id="email" 
-			    	   name="email"
-			    	   value="<?php if(isset($_GET['email']))
-			    						echo($_GET['email']); ?>" 
-			    	   placeholder="Enter email">
-			  </div>
-			  
-		  <button type="submit" 
+		   <button type="submit" 
 		          class="btn btn-primary"
-		          name ="create">Create</button>
-
-		</form>
+		          name="create">Create</button>
+		    <a href="read.php" class="link-primary">View</a>
+	    </form>
 	</div>
-
-
-
 </body>
 </html>
